2979b5cbe651fcc36d70bfae34411fb8b5e585af
0894777 <joey_gt@msn.com>
2016-01-26 15:22:45 +0100

    foto

--- Game_joey.py
+++ Game_joey.py
@@ -2,13 +2,18 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((1280,1024))
 screen.fill((80, 200, 250))
+background_position = [50, 50]
 pygame.display.set_caption('Survival game')
-img=pygame.image.load('help.png')
-done=False
+img = pygame.image.load('Button/GM/help.png')
+img =  pygame.transform.scale(img,(40,40))
+screen.blit(img,(1000,300))
 pygame.display.flip()
+
+done=False
 
 
 while not done:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         done = True
+                        pygame.display.update()
