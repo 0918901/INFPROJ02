fe8c4a0c39b997b247a51f8bdb12d2e5f704c39d
0898669 <0898669@hr.nl>
2016-01-27 17:53:45 +0100

    Test dobbelsteen functie

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from pygame.locals import *
 from sys import exit
 
@@ -7,7 +8,13 @@ bg        = 'Main/Game/wood.jpg'
 board       = 'Main/Game/board.png'
 lp          = 'Button/GM/levenspunten.png'
 cp          = 'Button/GM/conditiepunten.png'
-ds          = 'Main/Dice/D1.png'
+ds          = 'Main/Dice/D0.png'
+ds1          = 'Main/Dice/D1.png'
+ds2          = 'Main/Dice/D2.png'
+ds3          = 'Main/Dice/D3.png'
+ds4          = 'Main/Dice/D4.png'
+ds5          = 'Main/Dice/D5.png'
+ds6          = 'Main/Dice/D6.png'
 sf          = 'Cards/SFC/SFC1.png'
 score       = 'Cards/SC/SC1.jpg'
 speler      = 'Player/Faces/S1.png'
@@ -51,8 +58,26 @@ Start = pygame.image.load(start)
 Start = pygame.transform.scale(Start,(40, 40))
 
 #dobbelsteen
-DS  = pygame.image.load(ds)
-DS = pygame.transform.scale(DS,(100, 100))
+#DS = pygame.image.load(ds)
+#DS = pygame.transform.scale(DS,(100, 100))
+
+#DS1 = pygame.image.load(ds1)
+#DS1 = pygame.transform.scale(DS1,(100, 100))
+
+#DS2  = pygame.image.load(ds2)
+#DS2  = pygame.transform.scale(DS2,(100, 100))
+
+#DS3 = pygame.image.load(ds3)
+#DS3 = pygame.transform.scale(DS3,(100, 100))
+
+DS4  = pygame.image.load(ds4)
+D4  = pygame.transform.scale(DS4,(100, 100))
+
+DS5  = pygame.image.load(ds5)
+D5 = pygame.transform.scale(DS5,(100, 100))
+
+DS6  = pygame.image.load(ds6)
+D6  = pygame.transform.scale(DS6,(100, 100))
 
 # knop roll
 Roll = pygame.image.load(roll)
@@ -73,8 +98,12 @@ Speler = pygame.transform.scale(Speler,(100, 100))
 red_Pion = pygame.image.load(pion1)
 red_Pion= pygame.transform.scale(red_Pion,(50, 50))
 
+
+
+
 #zolang de bovenstaande klopt
 while True:
+
     #ophalen van pygame event
     for event in pygame.event.get():
         #anders stop de pygame
@@ -84,19 +113,50 @@ while True:
             #sluit de pygame
             exit()
 
-    #locaties op het spelbord
-    screen.blit(BG, (0,0))
-    screen.blit(lifepoints, (20,15))
-    screen.blit(gameboard, (20,100))
-    screen.blit(CP,(300,15))
-    screen.blit(Quit,(950,15))
-    screen.blit(Start,(900,15))
-    screen.blit(DS,(900,80))
-    screen.blit(Roll,(900,200))
-    screen.blit(SF,(550,100))
-    screen.blit(Speler,(850,350))
-    screen.blit(red_Pion,(460,530))
-    screen.blit(Score,(550,440))
+            #locaties op het spelbord
+        screen.blit(BG, (0,0))
+        screen.blit(lifepoints, (20,15))
+        screen.blit(gameboard, (20,100))
+        screen.blit(CP,(300,15))
+        screen.blit(Quit,(950,15))
+        screen.blit(Start,(900,15))
+        #screen.blit(DS,(900,80))
+        screen.blit(Roll,(900,200))
+        screen.blit(SF,(550,100))
+        screen.blit(Speler,(850,350))
+        screen.blit(red_Pion,(460,530))
+        screen.blit(Score,(550,440))
+
+        pygame.display.update()
+
+        rolling = (random.randint(2,5))
+
+
+        DS  =  pygame.transform.scale(DS,(100, 100))
+        D2  = pygame.transform.scale(DS2,(100, 100))
+
+        if rolling==1:
+
+            DS4  = pygame.image.load(ds)
+            D = pygame.transform.scale(DS,(100, 100))
+            screen.blit(DS4,(900,80))
+            print('rolling dice1')
+
+        elif rolling==2:
+
+            DS2  = pygame.image.load(ds2)
+            D2  = pygame.transform.scale(DS2,(100, 100))
+            screen.blit(DS2,(900,80))
+            print('rolling dice2')
+
+
+        elif rolling==3:
+
+            DS3  = pygame.image.load(ds3)
+            D3  = pygame.transform.scale(D3,(100, 100))
+            screen.blit(DS3,(900,80))
+            print('rolling dice3')
+
+
 
     #Scherm vernieuwen bij verandering
-    pygame.display.update()
