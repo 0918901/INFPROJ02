72af244d1371df31d672e2aa5c719e5476b3c942
Reuben Domacasse <0918901@hr.nl>
2016-01-28 16:36:54 +0100

    Reuben_Game updated  (Spelbord) Reuben_SuperFighterCard aangemaakt  (SFC) SuperFighterCards converted

--- SuperFighterCard.py
+++ SuperFighterCard.py
@@ -1,8 +1,7 @@
-import random
 import pygame
+import random
 from pygame.locals import *
 from sys import exit
-
 
 SFCA  = 'Cards/SFC/SFCA.jpg'
 SFC1  = 'Cards/SFC/SFC1.jpg'
@@ -33,8 +32,6 @@ D5    = 'Main/Dice/D5.png'
 D6    = 'Main/Dice/D6.png'
 dob   = 'Button/GM/knop_roll.png'
 
-
-
 screen_width   = 768
 screen_height  = 1024
 
@@ -53,87 +50,85 @@ dob_height =   200
 dob_x      =   500
 dob_y      =   200
 
+# opstarten van pygame
+pygame.init()
 
-screen = pygame.display.set_mode((screen_height,screen_width), DOUBLEBUF, 32)
+# grootte van het scherm aangeven
+screen = pygame.display.set_mode((screen_height,screen_width))
 
 dob = pygame.image.load(dob)
 dob = pygame.transform.scale(dob, (dob_width, dob_height))
 
-D0 = pygame.image.load(D0).convert_alpha()
+D0 = pygame.image.load(D0)
 D = pygame.transform.scale(D0, (dice_width, dice_height))
 
-SFCA = pygame.image.load(SFCA).convert_alpha()
+SFCA = pygame.image.load(SFCA)
 SFC = pygame.transform.scale(SFCA, (sfc_width, sfc_height))
 
-
-player1_choice = 0
-
-print("---------------------------------------------------------------")
-#Naam opdracht
-print("Dobbelsteen dobbelen")
-print("---------------------------------------------------------------")
-
 while True:
+    # ophalen van pygame event
     for event in pygame.event.get():
-        #anders stop de pygame
+        # anders stop de pygame
         if event.type == QUIT:
-            #detect sluitknop
+            # detect sluitknop
             pygame.quit()
-            #sluit de pygame
+            # sluit de pygame
             exit()
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             (mouseX, mouseY) = pygame.mouse.get_pos()
-            print ("X =",mouseX, "Y =",mouseY)
+
             if mouseX >= dob_x and mouseY >= dob_y and mouseX <= (dob_x+100) and mouseY <= (dob_y+100):
                 print("je hebt de roll knop gevonden")
-            player1_choice = random.randint(1,6)
-            if player1_choice == 1:
-                D1 = pygame.image.load(D1).convert_alpha()
-                D = pygame.transform.scale(D1, (dice_width, dice_height))
-                SFC1 = pygame.image.load(SFC1).convert_alpha()
-                SFC = pygame.transform.scale(SFC1, (sfc_width, sfc_height))
-                print ("je hebt 1 gegooid")
+                player1_choice = random.randint(1,6)
+                if player1_choice == 1:
+                    D1 = pygame.image.load(D1)
+                    D = pygame.transform.scale(D1, (dice_width, dice_height))
+                    SFC1 = pygame.image.load(SFC1)
+                    SFC = pygame.transform.scale(SFC1, (sfc_width, sfc_height))
+                    print ("je hebt 1 gegooid")
 
-            elif player1_choice == 2:
-                D2 = pygame.image.load(D2).convert_alpha()
-                D = pygame.transform.scale(D2, (dice_width, dice_height))
-                SFC2 = pygame.image.load(SFC2).convert_alpha()
-                SFC = pygame.transform.scale(SFC2, (sfc_width, sfc_height))
-                print ("je hebt 2 gegooid")
+                elif player1_choice == 2:
+                    D2 = pygame.image.load(D2)
+                    D = pygame.transform.scale(D2, (dice_width, dice_height))
+                    SFC2 = pygame.image.load(SFC2)
+                    SFC = pygame.transform.scale(SFC2, (sfc_width, sfc_height))
+                    print ("je hebt 2 gegooid")
 
-            elif player1_choice == 3:
-                D3 = pygame.image.load(D3).convert_alpha()
-                D = pygame.transform.scale(D3, (dice_width, dice_height))
-                SFC3 = pygame.image.load(SFC3).convert_alpha()
-                SFC = pygame.transform.scale(SFC3, (sfc_width, sfc_height))
-                print ("je hebt 3 gegooid")
+                elif player1_choice == 3:
+                    D3 = pygame.image.load(D3)
+                    D = pygame.transform.scale(D3, (dice_width, dice_height))
+                    SFC3 = pygame.image.load(SFC3)
+                    SFC = pygame.transform.scale(SFC3, (sfc_width, sfc_height))
+                    print ("je hebt 3 gegooid")
 
-            elif player1_choice == 4:
-                D4 = pygame.image.load(D4).convert_alpha()
-                D = pygame.transform.scale(D4, (dice_width, dice_height))
-                SFC4 = pygame.image.load(SFC4).convert_alpha()
-                SFC = pygame.transform.scale(SFC4, (sfc_width, sfc_height))
-                print ("je hebt 4 gegooid")
+                elif player1_choice == 4:
+                    D4 = pygame.image.load(D4)
+                    D = pygame.transform.scale(D4, (dice_width, dice_height))
+                    SFC4 = pygame.image.load(SFC4)
+                    SFC = pygame.transform.scale(SFC4, (sfc_width, sfc_height))
+                    print ("je hebt 4 gegooid")
 
-            elif player1_choice == 5:
-                D5 = pygame.image.load(D5).convert_alpha()
-                D = pygame.transform.scale(D5, (dice_width, dice_height))
-                SFC5 = pygame.image.load(SFC5).convert_alpha()
-                SFC = pygame.transform.scale(SFC5, (sfc_width, sfc_height))
-                print ("je hebt 5 gegooid")
+                elif player1_choice == 5:
+                    D5 = pygame.image.load(D5)
+                    D = pygame.transform.scale(D5, (dice_width, dice_height))
+                    SFC5 = pygame.image.load(SFC5)
+                    SFC = pygame.transform.scale(SFC5, (sfc_width, sfc_height))
+                    print ("je hebt 5 gegooid")
 
-            elif player1_choice == 6:
-                D6 = pygame.image.load(D6).convert_alpha()
-                D = pygame.transform.scale(D6, (dice_width, dice_height))
-                SFC6 = pygame.image.load(SFC6).convert_alpha()
-                SFC = pygame.transform.scale(SFC6, (sfc_width, sfc_height))
-                print ("je hebt 6 gegooid")
+                elif player1_choice == 6:
+                    D6 = pygame.image.load(D6)
+                    D = pygame.transform.scale(D6, (dice_width, dice_height))
+                    SFC6 = pygame.image.load(SFC6)
+                    SFC = pygame.transform.scale(SFC6, (sfc_width, sfc_height))
+                    print ("je hebt 6 gegooid")
 
+
+
+    # locaties op het spelbord
     screen.blit(SFC,(sfc_x, sfc_y))
     screen.blit(D,(dice_x, dice_y))
     screen.blit(dob,(dob_x, dob_y))
 
-
-
+    # Scherm vernieuwen bij verandering
     pygame.display.update()
