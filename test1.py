60cdd7fac96b4451fe8a905884e824ead011fdbc
joey <joey_gt@msn.com>
2016-01-28 22:11:08 +0100

    oef

--- test1.py
+++ test1.py
@@ -1,13 +1,48 @@
 import pygame
+import random
+from pygame.locals import *
+from sys import exit
+
+# afbeeldingen locaties aangeven
+bg = 'Main/Game/wood.jpg'
+board = 'Main/Game/board.png'
+
 pygame.init()
-screen = pygame.display.set_mode((1280,1024))
-screen.fill((80, 200, 250))
-pygame.display.set_caption('Survival game')
-done=False
-pygame.display.flip()
+sound = pygame.mixer.Sound()
+sound.play()
+# grootte van het scherm aangeven
+screen = pygame.display.set_mode((1024, 768))
+BG = pygame.image.load(bg).convert()
+# achtergrondkleurscreen.fill((80, 200, 250))
+
+# Laden van afbeelding
+spelbord = pygame.image.load(board)
+
+# Hervormen van de afbeelding
+gameboard = pygame.transform.scale(spelbord, (500, 500))
 
 
-while not done:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        done = True
+while True:
+    # ophalen van pygame event
+    for event in pygame.event.get():
+        # anders stop de pygame
+        if event.type == QUIT:
+            # detect sluitknop
+            pygame.quit()
+            # sluit de pygame
+            exit()
+
+
+
+
+
+
+
+
+    # locaties op het spelbord
+    screen.blit(BG, (0, 0))
+    screen.blit(gameboard, (20, 100))
+
+
+    # Scherm vernieuwen bij verandering
+    pygame.display.update()
