2594814733371536bae2311deefcdbf67962baf7
0898669 <0898669@hr.nl>
2016-01-29 15:11:09 +0100

    Merge branch 'master' of /Users/quinnjansen/PycharmProjects/INFPROJ02 with conflicts.

--- InstructiePage_Quinn.py
+++ InstructiePage_Quinn.py
@@ -1,13 +1,13 @@
 # Reuben heeft het bijgewerkt
 #Bestanden importeren
 import pygame
-from pygame.locals import *
 from sys import exit
+from pygame import *
 
 
 #afbeeldingen inladen
-background_image    = 'Main/Instructions/SH.jpg'
-back               = 'Main/instructions/Kruis2.png'
+background_image   = 'Main/Instructions/SH.jpg'
+terug               = 'Main/instructions/Kruis2.png'
 
 #Grootte van scherm aangeven
 screen_width        = 768
@@ -17,6 +17,9 @@ screen_height       = 1024
 RodeKnopje_width        = 40
 RodeKnopje_height       = 40
 
+tk_x = 915
+tk_y = 15
+
 #venster aanroepen met dubble buffer [extra snelheid](sneller laden)
 screen = pygame.display.set_mode((screen_height,screen_width))
 
@@ -25,7 +28,7 @@ background_image = pygame.image.load(background_image)
 background_image = pygame.transform.scale(background_image, (screen_height,screen_width))
 
 #Play knop aanroepen en vervormen naar aangegeven grootte [boven aangegeven](regel 18-19)
-terug_knop = pygame.image.load(back)
+terug_knop = pygame.image.load(terug)
 terug_knop = pygame.transform.scale(terug_knop, (RodeKnopje_width , RodeKnopje_height))
 
 
@@ -48,20 +51,15 @@ while True:
             print ("X =",mouseX, "Y =",mouseY)
 
             #als muis zich bevindt tussen x & y as
-            if mouseX >= 915 and mouseY >= 15 and mouseX <= 955 and mouseY <= 55:
+            if mouseX >= tk_x and mouseY >= tk_y and mouseX <= tk_x + RodeKnopje_width  and mouseY <= tk_y +RodeKnopje_height :
                 #print dat de play knop is vonden (muisdetectie) [controle]
-                print("je hebt de play knop gevonden")
+                print("je hebt de terug knop gevonden")
                 #Haal informatie op van Python file
                 from Game_joey import *
 
-
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            #Haal informatie op van Python file
-            from Game_Reuben import *
-
     #toon in venster (image, (x, y))
     screen.blit(background_image, (0, 0))
-    screen.blit(terug_knop,(950,15))
+    screen.blit(terug_knop,(tk_x,tk_y))
 
     #vernieuw de venster op verandering
     pygame.display.update()
