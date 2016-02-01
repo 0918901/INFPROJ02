a6af53c3cbf4f34dab31ff9cf0ee305f045537d5
0894777 <joey_gt@msn.com>
2016-02-01 10:41:04 +0100

    test

---
+++ move player.py
@@ -0,0 +1,91 @@
+import pygame
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption('speler')
+black = (0,0,0)
+white = (50,255,255)
+#foto laden
+carImg = pygame.image.load('Player/Piece/Blauw.png')
+carImg= pygame.transform.scale(carImg, (100, 100))
+def car(x,y):
+    gameDisplay.blit(carImg, (x,y))
+
+
+
+pygame.display.update()
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+
+pygame.mouse.set_visible(0)
+# Speed in pixels per frame
+x_speed = 0
+y_speed = 0
+
+# Current position
+x_coord = 10
+y_coord = 10
+
+# -------- Main Program Loop -----------
+while not done:
+    # --- Event Processing
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+            # User pressed down on a key
+
+        elif event.type == pygame.KEYDOWN:
+            # Figure out if it was an arrow key. If so
+            # adjust speed.
+            if event.key == pygame.K_LEFT:
+                x_speed = -3
+
+            elif event.key == pygame.K_RIGHT:
+                x_speed = 3
+            elif event.key == pygame.K_UP:
+                y_speed = -3
+            elif event.key == pygame.K_DOWN:
+                y_speed = 3
+
+        # User let up on a key
+        elif event.type == pygame.KEYUP:
+            # If it is an arrow key, reset vector back to zero
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_speed = 1
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                y_speed = 1
+
+    # --- Game Logic
+    pygame.mouse.set_visible(0)
+    # Move the object according to the speed vector.
+    x_coord = x_coord + x_speed
+    y_coord = y_coord + y_speed
+
+    # --- Drawing Code
+
+    # First, clear the screen to WHITE. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+    gameDisplay.fill(GREEN)
+    car(x_coord,y_coord)
+
+
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # Limit frames per second
+    clock.tick(60)
+
+# Close the window and quit.
+pygame.quit()
