b426bd8df3a4bc18e9022f88c49f9ae19d8d232d
0918901 <0918901@hr.nl>
2016-02-03 21:12:24 +0100

    Game file updated (Lose State)

--- Game.py
+++ Game.py
@@ -134,12 +134,9 @@ def Intro():
 
     pygame.init()
 
-
-    music = 1
     pygame.mixer.music.load('Sound/startmenu.mp3')
     pygame.mixer.music.set_endevent(pygame.constants.USEREVENT)
-    if music == 1:
-        pygame.mixer.music.play()
+    pygame.mixer.music.play()
 
     players = 0
     while True:
@@ -748,11 +745,9 @@ def Game():
     SFCA = pygame.image.load('Cards/SFC/SFCA.png')
     SFC = pygame.transform.scale(SFCA, (sfc_width, sfc_height))
 
-    music = 1
     pygame.mixer.music.load(os.path.join('Sound/startmenu.mp3'))
     pygame.mixer.music.set_endevent(pygame.constants.USEREVENT)
-    if music == 1:
-        pygame.mixer.music.play()
+    pygame.mixer.music.play()
     dice_num2 = 0
 
     while True:
@@ -1227,6 +1222,7 @@ def Game():
                 print("je hebt op 3 gedrukt")
                 if players >= 1:
                     players = 4
+                    Winner()
 
 
         font = pygame.font.Font(None, 36)
@@ -1685,23 +1681,13 @@ def Gamerules():
 def Winner():
     pygame.init()
 
-    screen = pygame.display.set_mode((800,800))
-
     pygame.display.set_caption("Animation")
-    terugNaarMenu               = 'Main/instructions/Kruis2.png'
     tryAgain_knop               = 'Main/Game/winKnop.png'
-    # spelbord sprites
-    bs = 'Main/Game/achtergrondWinLose.png'
 
     clock = pygame.time.Clock()
+    screen_height     = 768
+    screen_width      = 1024
 
-    #Grootte van terugKnop aangeven
-    RodeKnopje_width        = 30
-    RodeKnopje_height       = 25
-
-    #positie van terugKnop
-    tk_x = 970
-    tk_y = 38
 
     #Grootte van tryAgainknop aangeven
     tryKnopje_width        = 150
@@ -1712,24 +1698,17 @@ def Winner():
     try_y = 540
 
     #Play knop aanroepen en vervormen naar aangegeven grootte [boven aangegeven](regel 18-19)
-    terug_knop = pygame.image.load(terugNaarMenu)
-    terug_knop = pygame.transform.scale(terug_knop, (RodeKnopje_width , RodeKnopje_height))
-
-    #Play knop aanroepen en vervormen naar aangegeven grootte [boven aangegeven](regel 18-19)
     tryy = pygame.image.load(tryAgain_knop)
     tryy = pygame.transform.scale(tryy, (tryKnopje_width , try_height))
 
 
-    w1 = pygame.image.load("Main/Game/winSituatieBlauw.png")
-    w1 = pygame.transform.scale(w1, (1024, 728))
-    w2 = pygame.image.load("Main/Game/winSituatieRed.png")
-    w2 = pygame.transform.scale(w2, (1024, 728))
+    w1 = pygame.image.load("Main/Game/winSituatieBlauw2.png")
+    w1 = pygame.transform.scale(w1, (screen_width, screen_height))
+    w2 = pygame.image.load("Main/Game/winSituatieRed2.png")
+    w2 = pygame.transform.scale(w2, (screen_width, screen_height))
 
 
-    screen = pygame.display.set_mode((1024, 728))
-    BG = pygame.image.load(bs).convert()
-
-    screen.blit(BG, (0, 0))
+    screen = pygame.display.set_mode((screen_width, screen_height))
 
 
     winnerCurrentImage = 1
@@ -1746,7 +1725,6 @@ def Winner():
         if (winnerCurrentImage==1):
 
             screen.blit(w1, (0,0))
-            screen.blit(terug_knop,(tk_x,tk_y))
             screen.blit(tryy,(try_x,try_y))
             if event.type == pygame.MOUSEBUTTONDOWN:
                 (mouseX, mouseY) = pygame.mouse.get_pos()
@@ -1761,7 +1739,6 @@ def Winner():
         if (winnerCurrentImage==2):
 
             screen.blit(w2, (0,0))
-            screen.blit(terug_knop,(tk_x,tk_y))
             screen.blit(tryy,(try_x,try_y))
             if event.type == pygame.MOUSEBUTTONDOWN:
                 (mouseX, mouseY) = pygame.mouse.get_pos()
@@ -1777,7 +1754,6 @@ def Winner():
 
             winnerCurrentImage=1
 
-            screen.blit(terug_knop,(tk_x,tk_y))
             screen.blit(tryy,(try_x,try_y))
             if event.type == pygame.MOUSEBUTTONDOWN:
                 (mouseX, mouseY) = pygame.mouse.get_pos()
@@ -1802,21 +1778,7 @@ def Winner():
 def Loser():
     pygame.init()
 
-    screen = pygame.display.set_mode((800,800))
-
     tryAgain_knop               = 'Main/Game/winKnop.png'
-    # spelbord sprites
-    bs = 'Main/Game/achtergrondWinLose.png'
-
-    clock = pygame.time.Clock()
-
-    #Grootte van terugKnop aangeven
-    RodeKnopje_width        = 30
-    RodeKnopje_height       = 25
-
-    #positie van terugKnop
-    tk_x = 970
-    tk_y = 38
 
     #Grootte van tryAgainknop aangeven
     tryKnopje_width        = 200
@@ -1834,26 +1796,14 @@ def Loser():
     w1 = pygame.image.load("Main/Game/lose.png")
     w1 = pygame.transform.scale(w1, (1024, 728))
 
-
     screen = pygame.display.set_mode((1024, 728))
-    BG = pygame.image.load(bs).convert()
-
-    screen.blit(BG, (0, 0))
 
 
-    winnerCurrentImage = 1
 
-    gameLoop=True
-    while gameLoop:
 
+
+    while True:
         for event in pygame.event.get():
-
-            if (event.type==pygame.QUIT):
-
-                gameLoop=False
-
-        if (winnerCurrentImage==1):
-
             screen.blit(w1, (0,0))
             screen.blit(tryy,(try_x,try_y))
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -1866,15 +1816,6 @@ def Loser():
                     print("je hebt de terug knop gevonden")
                     Intro()
 
-        else:
-
-            winnerCurrentImage+=1;
-
-        pygame.display.flip()
-
-        clock.tick(5)
-
-    pygame.quit()
-
+            pygame.display.update()
 
 Intro()
